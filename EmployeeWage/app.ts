5925d010c9b2dfcffc21c219ec28808c0ce0db14
PalakPartani <partanipalak16@gmail.com>
2020-08-26 09:15:33 -0700

    [Palak]MODIFIED:Using switch statement

--- EmployeeWage/app.ts
+++ EmployeeWage/app.ts
@@ -6,15 +6,23 @@ let PART_TIME_HOUR = 4;
 
 let empWage = 0;
 let empCheck = Math.round(Math.random() * 10) % 3;
-if (empCheck == 1) {
-    var dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-    console.log('Employee daily Wage is : ' + dailyWage);
-}
-else if (empCheck == 0) {
-    console.log('Part Time Employee Is Present');
-    var dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-    console.log('Part Time Employee Wage is : ' + dailyWage);
-}
-else {
-    console.log('Employee Is Absent');
+switch (empCheck) {
+    case 0:
+        {
+            console.log('Full Time Employee Is Present');
+            var dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            console.log('Employee daily Wage is : ' + dailyWage);
+            break;
+        }
+    case 1:
+        {
+            console.log('Part Time Employee Is Present');
+            var dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+            console.log('Part Time Employee Wage is : ' + dailyWage);
+            break;
+        }
+    default:
+        {
+            console.log('Employee Is Absent');
+        }
 }
