8e7c5aeb62ec8cc16f75f939a9d04523b9990f11
PalakPartani <partanipalak16@gmail.com>
2020-08-26 09:10:14 -0700

    [Palak]ADDED:Feature to calculate employee daily wage

--- EmployeeWage/app.ts
+++ EmployeeWage/app.ts
@@ -1,4 +1,14 @@
-console.log('Hello world');
+
 let isPresent = 1;
+let empWorkingHrs = 0;
+let WAGE_PER_HOUR = 20;
+let FULL_DAY_HOUR = 8;
+let empWage = 0;
 let empCheck = Math.floor(Math.random() * 10) % 2;
-empCheck == 1 ? console.log("Employee is present") : console.log("Employee is absent");
+if (empCheck == 1) {
+    var dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+    console.log('Employee daily Wage is : ' + dailyWage);
+}
+else {
+    console.log('Employee Is Absent');
+}
