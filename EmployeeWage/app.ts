2e1efc2dad448a68209ad47f2170dce3904f6537
PalakPartani <partanipalak16@gmail.com>
2020-08-26 09:12:41 -0700

    [Palak]ADDED:Feature to get part time employee & wage

--- EmployeeWage/app.ts
+++ EmployeeWage/app.ts
@@ -1,13 +1,19 @@
-
 let isPresent = 1;
 let empWorkingHrs = 0;
 let WAGE_PER_HOUR = 20;
 let FULL_DAY_HOUR = 8;
+let PART_TIME_HOUR = 4;
+
 let empWage = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
+let empCheck = Math.round(Math.random() * 10) % 3;
 if (empCheck == 1) {
     var dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
     console.log('Employee daily Wage is : ' + dailyWage);
+}
+else if (empCheck == 0) {
+    console.log('Part Time Employee Is Present');
+    var dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+    console.log('Part Time Employee Wage is : ' + dailyWage);
 }
 else {
     console.log('Employee Is Absent');
