d8abcdb10bff969fc6d6191bb1823b3f951b118c
PalakPartani <partanipalak16@gmail.com>
2020-08-27 00:45:57 -0700

    [Palak]ADDED:Feature to calculate daily emp wage

--- EmployeeWage/app.ts
+++ EmployeeWage/app.ts
@@ -1,36 +1,48 @@
 //Declaring constants and variables.
-let WAGE_PER_HOUR = 20;
-let PART_TIME = 4;
-let FULL_TIME = 8;
+const WAGE_PER_HOUR = 20;
+const PART_TIME = 4;
+const FULL_TIME = 8;
 let totalSal = 0;
 let day = 1;
 let hours = 0;
-//Iterating for calculating wage for 20 days.
-while (day < 20 && hours < 100) {
-    let empCheck = Math.round(Math.random() * 10) % 3;
-    getWorkHours(empCheck);
-    day++;
+let hr = 0;
+//cal daily wage.
+let calculateDailyWage = (employeeHrs: number) => {
+    var wage = employeeHrs * WAGE_PER_HOUR;
+    console.log("Daily Wage : " + wage)
 }
-
     //Function to get working hours.
-    function getWorkHours(value:number){
+    let getWorkHours=(value:number)=>{
         switch (value) {
             case 0:
-                console.log('Full Time Employee Is Present');
                 totalSal += WAGE_PER_HOUR * FULL_TIME;
-                hours += FULL_TIME;
+                hours = FULL_TIME;
                 break;
             case 1:
-                console.log('Part Time Employee Is Present');
+               // console.log('Part Time Employee is Present');
                 totalSal += WAGE_PER_HOUR * PART_TIME;
-                hours += PART_TIME;
+                hours = PART_TIME;
+
                 break;
             default:
-                console.log('Employee Is Absent');
+                console.log('Employee is Absent');
                 totalSal +=0;
                 hours += 0;
         }
+
+
+}
+
+//Iterating for calculating wage for 20 days or 100 hours.
+while (day < 20 && hr < 100) {
+    day++;
+    let empCheck = Math.round(Math.random() * 10) % 3;
+        getWorkHours(empCheck);
+        hr += hours;
+    var empDailyWage: Array<number> = [hr];
+    calculateDailyWage(hours);
+   
 }
 //Displaying the total salary for month.
-console.log('Employee total Wage is : ' + totalSal);
-console.log('Working Hrs :' + hours);
+totalSal = (hr * WAGE_PER_HOUR);
+console.log("Wages for a Month :  " + totalSal);
