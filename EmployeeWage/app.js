9bee3cec2f77cf42c8482be3a28e7657d0ea35a3
PalakPartani <partanipalak16@gmail.com>
2020-08-26 19:47:26 -0700

    [Palak]ADDED:Feature to calculate wage until condition satisfied

--- EmployeeWage/app.js
+++ EmployeeWage/app.js
@@ -1,27 +1,33 @@
-let isPresent = 1;
-let empWorkingHrs = 0;
+//Declaring constants and variables.
 let WAGE_PER_HOUR = 20;
 let NO_OF_WORKING_DAYS = 20;
 let PART_TIME = 4;
 let FULL_TIME = 8;
 let totalSal = 0;
 let day = 1;
-while (day < 20) {
+let hours = 0;
+//Iterating for calculating wage for 20 days.
+while (day < 20 && hours < 100) {
     let empCheck = Math.round(Math.random() * 10) % 3;
     switch (empCheck) {
         case 0:
             console.log('Full Time Employee Is Present');
             totalSal = (totalSal + (WAGE_PER_HOUR * FULL_TIME));
+            hours += FULL_TIME;
             break;
         case 1:
             console.log('Part Time Employee Is Present');
             totalSal = (totalSal + (WAGE_PER_HOUR * PART_TIME));
+            hours += PART_TIME;
             break;
         case 2:
             console.log('Employee Is Absent');
             totalSal = totalSal + 0;
+            hours += 0;
     }
     day++;
 }
-console.log('Employee daily Wage is : ' + totalSal);
+//Displaying the total salary for month.
+console.log('Employee total Wage is : ' + totalSal);
+console.log('Working Hrs :' + hours);
 //# sourceMappingURL=app.js.map
--- EmployeeWage/app.ts
+++ EmployeeWage/app.ts
@@ -5,24 +5,28 @@ let PART_TIME = 4;
 let FULL_TIME = 8;
 let totalSal = 0;
 let day = 1;
-
+let hours = 0;
 //Iterating for calculating wage for 20 days.
-while (day < 20) {
+while (day < 20 && hours <100) {
     let empCheck = Math.round(Math.random() * 10) % 3;
     switch (empCheck) {
         case 0:
-                console.log('Full Time Employee Is Present');
-                totalSal = (totalSal + (WAGE_PER_HOUR * FULL_TIME));
-                break;
+            console.log('Full Time Employee Is Present');
+            totalSal = (totalSal + (WAGE_PER_HOUR * FULL_TIME));
+            hours += FULL_TIME;
+            break;
         case 1:
-                console.log('Part Time Employee Is Present');
-                totalSal = (totalSal + (WAGE_PER_HOUR * PART_TIME));
-                break;
+            console.log('Part Time Employee Is Present');
+            totalSal = (totalSal + (WAGE_PER_HOUR * PART_TIME));
+            hours += PART_TIME;
+            break;
         case 2:
             console.log('Employee Is Absent');
-            totalSal = totalSal + 0;  
+            totalSal = totalSal + 0;
+            hours += 0;
     }
     day++;
 }
 //Displaying the total salary for month.
 console.log('Employee total Wage is : ' + totalSal);
+console.log('Working Hrs :' + hours);
